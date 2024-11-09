833cb865d6a5b8a017e0df3482ea41b1a1fc83d5
radkinz <radkins@mit.edu>
2024-11-08 20:18:12 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("submit");
+
+const USERNAME = "brain";
+const PASSWORD = "damage";
+const ADDRESS = "518 Beacon Street";
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    //grab username and password submitted
+    username_submitted = document.getElementById("username").value;
+    password_submiited = document.getElementById("password").value;
+
+    if (username_submitted == USERNAME && password_submiited == PASSWORD) {
+        modal.style.display = "none"
+        document.getElementById('address').innerHTML = "<h2>"+ ADDRESS +"</h2>";
+    } else {
+        alert("Are you sure you're on the right site?")
+    }
+}
